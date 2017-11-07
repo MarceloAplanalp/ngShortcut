43508f882d1f251fb574bf098bc2ef272e775d32
Marcelo Aplanalp <marceaplanalp@gmail.com>
2017-11-07 00:25:33 -0300

    fix tests

--- src/app/services/shortcut.service.spec.ts
+++ src/app/services/shortcut.service.spec.ts
@@ -1,15 +1,19 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { ShortcutService } from './shortcut.service';
+import { WindowService } from './window.service';
 
 describe('ShortcutService', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [ShortcutService]
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [
+                ShortcutService,
+                { provide: WindowService, useValue: window }
+            ]
+        });
     });
-  });
 
-  it('should be created', inject([ShortcutService], (service: ShortcutService) => {
-    expect(service).toBeTruthy();
-  }));
+    it('should be created', inject([ShortcutService], (service: ShortcutService) => {
+        expect(service).toBeTruthy();
+    }));
 });
